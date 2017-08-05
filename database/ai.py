46c47691e93e8aa60aa5931ca6bcb1dcfaec399a
DarthSergeant <darthsergeant@gmail.com>
2017-08-05 09:10:38 -0400

    Update ai.py

--- database/ai.py
+++ database/ai.py
@@ -25,7 +25,18 @@ def create_response(sentence):
         msg = data['hay']
     if sentence == "!wel":
         msg = data['wel']
-
+    
+    #8 Ball
+    eight_ball = ['It is certain', ' It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
+              'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
+              'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now',
+              'Concentrate and ask again', 'Dont count on it', 'My reply is no', 'My sources say no',
+              'Outlook not so good', 'Very doubtful']
+    
+    if "!8ball" in sentence:
+        num = random.randint(0, (len(eight_ball)-1))
+        msg = eight_ball[num]
+        
     #Cat Facts
     if sentence == '!catfacts':
         num = random.randint(0, (len(catfacts)-1))
