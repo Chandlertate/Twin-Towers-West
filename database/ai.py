dd666e879eea17038f205b60114bc4cd8077a5fe
DarthSergeant <darthsergeant@gmail.com>
2017-08-15 11:15:12 -0400

    Update ai.py

--- database/ai.py
+++ database/ai.py
@@ -27,8 +27,8 @@ def create_response(sentence):
         msg = data['wel']
     
     #Call All
-    if "@holderby" in sentence:
-        msg = "@Meena elango @Gabriel Poindexter"
+    #if "@holderby" in sentence:
+     #   msg = "@Meena elango @Gabriel Poindexter"
     #8 Ball
     eight_ball = ['It is certain', ' It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
               'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes',
