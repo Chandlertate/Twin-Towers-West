37eda7aac8d5f470beb47fa1afe60b782c411102
Chandlertate <31077130+Chandlertate@users.noreply.github.com>
2017-09-21 14:31:15 -0400

    Update ai.py

--- database/ai.py
+++ database/ai.py
@@ -4,27 +4,6 @@ from database.cat_facts import catfacts
 
 def create_response(sentence):
     msg = {}
-    #Residence Halls
-    if sentence == "!hy":
-        msg = data['hy']
-    if sentence == "!bus":
-        msg = data['bus']
-    if sentence == "!ttw":
-        msg = data['ttw']
-    if sentence == "!tte":
-        msg = data['tte']
-    if sentence == "!fyn":
-        msg = data['fyn']
-    if sentence == "!fys":
-        msg = data['fys']
-    if sentence == "!gib":
-        msg = data['gib']
-    if sentence == "!wil":
-        msg = data['wil']
-    if sentence == "!hay":
-        msg = data['hay']
-    if sentence == "!wel":
-        msg = data['wel']
     
     #8 Ball
     eight_ball = ['It is certain', ' It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it',
@@ -52,15 +31,4 @@ def create_response(sentence):
 
     return msg
 
-data = {
-          'hy' : os.getenv('HY'),
-          'bus' : os.getenv('BUS'),
-          'ttw' : os.getenv('TTW'),
-          'tte' : os.getenv('TTE'),
-          'fyn' : os.getenv('FYN'),
-          'fys' : os.getenv('FYS'),
-          'gib' : os.getenv('GIB'),
-          'wil' : os.getenv('WIL'),
-          'hay' : os.getenv('HAY'),
-          'wel' : os.getenv('WEL'),
-}
+
